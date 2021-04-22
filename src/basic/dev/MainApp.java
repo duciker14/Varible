a0ddaea335d213e295e03d1d673f1ab2b5602a21
duciker14 <duciker14@gmail.com>
2021-04-22 14:11:32 +0700

    first commit

--- src/basic/dev/MainApp.java
+++ src/basic/dev/MainApp.java
@@ -1,9 +1,30 @@
 package basic.dev;
+/**
+ * 
+ * @author LAPTOP KIM ANH
+ *
+ */
 
 public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/**
+		 * Khai bao bien
+		 */
+		String firstName = "Phan Van";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		
+		System.out.println("Ho va ten:");
+		System.out.println(firstName + " " + lastName );
+		System.out.println("Mark Math: " + markMath);
+		System.out.println("Passed: " + passed);
+		
+		//Khai bao hang. hang phai viet hoa
+		final float PI = 3.14f;
 
 	}
 
